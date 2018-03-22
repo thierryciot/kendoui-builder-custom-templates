b1b1f31f76718c7f3e2584f5efcd64c6a1db7b41
Thierry Ciot <thciot@gmail.com>
2018-03-22 14:11:10 -0400

    Initial commit for hello world custom view V2.  This one builds on V1 by adding event handling: it simply supports click on salutation DIV element.

---
+++ views/custom-hello-world-v2/design-time/generator/index.js
@@ -0,0 +1,14 @@
+(function(module) {
+    'use strict';
+    console.log("Trace: in design time generator" );
+
+    module.exports = {
+        augmentModel: function(metaModel) {
+            // try {
+            // }
+            // catch ( e ) {
+            //     console.log("ERROR in augmentModel" + e );
+            // }
+        }
+    };
+})(module);
