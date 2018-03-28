07bdedc66d4a9f12e6eb76767c6099512b380b80
Thierry Ciot <thciot@gmail.com>
2018-03-28 14:36:01 -0400

    I18N process: Showing how we can 1) provide the keys and values to augment the default translation file with custom view title 2) how we could provide a translation to view properties (in our case the greeting property)

--- views/custom-hello-world-v1/angularjs/generator/index.js
+++ views/custom-hello-world-v1/angularjs/generator/index.js
@@ -14,10 +14,32 @@
         augmentModel(metaModel, metaPath) {
             var options = {
             };
+            //  we are building a unique key like this modules.HelloWorld.views.HWV1.label
+            // provide keys for properties.
+            const keyPrefix = `modules.${ metaModel.moduleName }.views.${ metaModel.name }`;
+            metaModel.labelKey = `${keyPrefix}.label`;
+            metaModel.titleKey = `${keyPrefix}.title`;
+            // This is how we would provide a translation to view properties
+            metaModel.greetingKey = `${keyPrefix}.greeting`;
+        }
+
+        getTranslation(view)
+        {
+            // provide default values for properties
+            //const editMessages = Object.assign(view.editOptions.toolbarButtonLabels, view.editOptions.rowButtonLabels);
+            const translation = {
+                label: view.label || view.name,
+                title: view.title,
+                greeting: view.greeting
+            };
+
+            return translation;
         }
     }
 
-    module.exports = (mb, pretty) => {
+
+
+        module.exports = (mb, pretty) => {
         return new Generator(mb, pretty);
     };
 
