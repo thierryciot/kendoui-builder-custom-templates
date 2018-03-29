b180cb2232825b0de5a791848b415d25b7b56749
Thierry Ciot <thciot@gmail.com>
2018-03-29 10:21:44 -0400

    First pass at a simple list rendering from data source. It's not working yet.  Need to debug why augmentModel is not called (maybe a bug in kuib?)

---
+++ views/custom-simple-list/design-time/generator/index.js
@@ -0,0 +1,31 @@
+(function(module) {
+    'use strict';
+    console.log("Trace: in design time generator" );
+
+    module.exports = {
+        augmentModel: function(metaModel) {
+
+            metaModel.title = 'tetetete';
+            const sampleData =
+                [ { 'id': 1, 'name': 'test 1' }
+                    , { 'id': 2, 'name': 'test 2' }
+                ];
+            metaModel.test = 'dasd';
+            metaModel.sampleData = sampleData;
+            // try {
+            // }
+            // catch ( e ) {
+            //     console.log("ERROR in augmentModel" + e );
+            // }
+        }
+        // ,
+        // getTranslation: function(view) {
+        //     try {
+        //         console.log( `* In getTranslation for ${view.label}`);
+        //     }
+        //     catch ( e ) {
+        //         console.log("ERROR in getTranslation" + e );
+        //     }
+        // }
+    };
+})(module);
