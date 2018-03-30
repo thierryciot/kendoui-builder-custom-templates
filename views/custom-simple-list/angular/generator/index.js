307ac2141b760752fcc231059f0ee010bd08e337
Thierry Ciot <thciot@gmail.com>
2018-03-30 06:35:24 -0400

    Initial commit on illustrating the different way to get at model data (augmentModel, config.json.ejs) Not working yet but need to checkin to transfer to different directory for kuib v3.

--- views/custom-simple-list/angular/generator/index.js
+++ views/custom-simple-list/angular/generator/index.js
@@ -3,6 +3,19 @@
 
     class Generator {
         // can use augment model too.
+        /*
+         * Adds or removes properties from a meta model
+         * @param {object} metaModel - The metaModel being processed, this could be either view or ui component.
+         * @param {string} metaPath - The path to the folder containing the meta information for the application.
+         */
+        augmentModel(metaModel, metaPath) {
+            var myList3 =
+                [ { 'id': 1, 'name': 'test 1 (sample data from index.js augmentModel)' }
+                    , { 'id': 2, 'name': 'test 2' }
+                ];
+
+            metaModel.myList3 = myList3;
+        }
 
         getViewChildComponents(view) {
         }
