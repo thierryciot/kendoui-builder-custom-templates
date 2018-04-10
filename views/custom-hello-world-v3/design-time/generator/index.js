1806ee899a20f3710d303562893488229cdba694
Thierry Ciot <thciot@gmail.com>
2018-04-10 13:55:07 -0400

    HW3: bring up to par with HW1

--- views/custom-hello-world-v3/design-time/generator/index.js
+++ views/custom-hello-world-v3/design-time/generator/index.js
@@ -1,6 +1,7 @@
 (function(module) {
     'use strict';
     // Right now there is no direct access to the generator output.
+    // However, if you start KUIB from the command line, you will be able to see console messages.
     // console.log("Trace: in design time generator" );
 
     module.exports = {
