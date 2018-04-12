a4f4987907a8b76403632a11eb491972b6c08100
thciot <thciot@gmail.com>
2018-04-12 14:10:26 -0400

    HW2: work in progress

--- views/custom-hello-world-v2/design-time/generator/index.js
+++ views/custom-hello-world-v2/design-time/generator/index.js
@@ -3,13 +3,13 @@
     console.log("Trace: in design time generator" );
 
     module.exports = {
-        augmentModel: function(metaModel) {
-            // try {
-            // }
-            // catch ( e ) {
-            //     console.log("ERROR in augmentModel" + e );
-            // }
-        }
+        // augmentModel: function(metaModel) {
+        //     try {
+        //     }
+        //     catch ( e ) {
+        //         console.log("ERROR in augmentModel" + e );
+        //     }
+        // }
         // ,
         // getTranslation: function(view) {
         //     try {
